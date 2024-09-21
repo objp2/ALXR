cc5192cbaa654542555c5476d8a488b735558e79
korejan <64199710+korejan@users.noreply.github.com>
2024-09-21 16:16:01 +0100

    remove android project / gradle build scripts for cmake only builds
    * Removes use of gradle in build-script.rs, not needed for build a purely native shared library and unifies code path to one cmake-only way of building the library
        * This change also fixes the issue of android builds on windows with openjdk compiler hangs indefinitely without running android-studio.
    
    * Bump android compile-sdk from v34 to v35
    
    * Bump android cmake from v3.22.0 to v3.30.3

--- alvr/openxr-client/alxr-engine-sys/build.rs
+++ alvr/openxr-client/alxr-engine-sys/build.rs
@@ -33,40 +33,6 @@ fn android_abi_name(target_arch: &target_lexicon::Triple) -> Option<&'static str
     }
 }
 
-fn gradle_task_from_profile(profile: &str, product_flavor: &str) -> String {
-    let build_type = if profile.to_lowercase() == "debug" {
-        "Debug"
-    } else {
-        "Release"
-    };
-    format!("assemble{0}{1}", product_flavor, build_type)
-}
-
-fn gradle_build_out_dir(
-    target_triple: &target_lexicon::Triple,
-    profile: &str,
-    product_flavor: &str,
-) -> PathBuf {
-    PathBuf::from(format!(
-        "intermediates/library_and_local_jars_jni/{0}{1}/jni/{2}",
-        //"intermediates/stripped_native_libs/{0}{1}/out/lib/{2}",
-        product_flavor,
-        if profile == "debug" {
-            "Debug"
-        } else {
-            "Release"
-        }, // TODO: Fix case senstive folder names!
-        android_abi_name(&target_triple).unwrap()
-    ))
-}
-
-fn gradle_cmd(operating_system: target_lexicon::OperatingSystem) -> &'static str {
-    match operating_system {
-        OperatingSystem::Windows => "gradlew.bat",
-        _ => "gradlew",
-    }
-}
-
 fn is_android_env(target_triple: &target_lexicon::Triple) -> bool {
     match target_triple.environment {
         Environment::Android | Environment::Androideabi => return true,
@@ -78,14 +44,6 @@ fn is_feature_enabled(feature_name: &str) -> bool {
     // yeah I know this is potentially very slow, sort this out later...
     env::vars().find(|(k, _)| feature_name == k).is_some()
 }
-
-// fn cmake_option_from_env_var(var_name : &str) -> &'static str {
-//     let enabled = env::var(var_name)
-//                     .unwrap_or_default()
-//                     .parse::<u32>()
-//                     .unwrap_or(0) > 0;
-//     if enabled { "ON" } else { "OFF" }
-// }
 
 fn cmake_option_from_bool(flag: bool) -> &'static str {
     if flag {
@@ -129,8 +87,10 @@ const ENV_VAR_MONITOR_LIST: [&'static str; 2] = [CMAKE_GEN_ENV_VAR, BUNDLE_FFMPE
 
 fn main() {
     let target_triple = Triple::from_str(&env::var("TARGET").unwrap()).unwrap();
+    let host_triple = Triple::from_str(&env::var("HOST").unwrap()).unwrap();
     let profile = env::var("PROFILE").unwrap();
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir_str = env::var("OUT_DIR").unwrap();
+    let out_dir = PathBuf::from(&out_dir_str);
     let project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     assert!(project_dir.ends_with("alxr-engine-sys"));
 
@@ -182,70 +142,100 @@ fn main() {
         .map(DirEntry::into_path)
         .collect::<Vec<_>>();
 
+    let default_generator = "Ninja";
+    let cmake_generator = env::var(CMAKE_GEN_ENV_VAR)
+        .map(|s| {
+            if s.is_empty() {
+                String::from(default_generator)
+            } else {
+                s
+            }
+        })
+        .unwrap_or(String::from(default_generator));
+
+    let mut config = Config::new("cpp/ALVR-OpenXR-Engine");
+    if target_triple.vendor != target_lexicon::Vendor::Uwp {
+        assert!(!cmake_generator.is_empty());
+        config.generator(&cmake_generator);
+    } else {
+        // Using Ninja to build UWP/WinStore apps fails to build.
+        // This should not be neccessary if the enviroment is
+        // setup correctly (i.e. using vcvarsall.bat) for building UWP/WinStore apps,
+        // and VS's fork of cmake.
+        define_windows_store(&mut config);
+    }
+
+    config
+        .always_configure(true)
+        .define("BUILD_ALL_EXTENSIONS", "ON")
+        .define("BUILD_API_LAYERS", "OFF")
+        .define("BUILD_TESTS", "OFF")
+        .define("BUILD_CONFORMANCE_TESTS", "OFF")
+        .define(
+            "USE_OCULUS_OXR_EXT_HEADERS",
+            cmake_option_from_feature(&ENABLE_OCULUS_EXT_HEADERS_FEATURE),
+        )
+        .define(
+            "DISABLE_DECODER_SUPPORT",
+            cmake_option_from_feature(&DISABLE_DECODER_FEATURE),
+        );
+    if profile == "release" && cmake_generator == "Ninja" {
+        config.build_target("install/strip");
+    }
+
     let alxr_engine_output_dir = if is_android_env(&target_triple) {
         let product_flavor = get_product_flavour();
         println!("selected product flavor: {0}", product_flavor);
-        let gradle_output_dir = out_dir.join("gradle_build");
-        let gradle_cmd_path = android_dir.join(gradle_cmd(target_lexicon::HOST.operating_system));
-        let status = Command::new(gradle_cmd_path)
-            .arg(format!(
-                "-PbuildDir={}",
-                gradle_output_dir.to_string_lossy()
-            ))
-            .arg(format!(
-                "-PABI={}",
-                android_abi_name(&target_triple).unwrap()
-            ))
-            .arg(gradle_task_from_profile(&profile, &product_flavor))
-            .current_dir(&android_dir)
-            .status()
-            .unwrap();
-        if !status.success() {
-            panic!("gradle failed to build libalxr_engine.so");
+        match product_flavor {
+            "PicoMobileOXR" => {
+                config
+                    .define("BUILD_LOADER", "OFF")
+                    .define("USE_PICO_MOBILE_LOADER", "ON");
+            }
+            _ => {
+                config.define("BUILD_LOADER", "ON");
+            }
+        };
+
+        if host_triple.operating_system != OperatingSystem::Windows {
+            // GNUInstallDirs vars workaround...
+            config
+                .define("CMAKE_INSTALL_INCLUDEDIR", out_dir_str.clone() + "/include")
+                .define("CMAKE_INSTALL_LIBDIR", out_dir_str.clone() + "/lib");
         }
-        let bin_dir_rel = gradle_build_out_dir(&target_triple, &profile, &product_flavor);
-        gradle_output_dir.join(&bin_dir_rel)
+
+        let ndk_home = env::var("ANDROID_NDK_HOME").unwrap();
+        let cmake_toolchain_file = ndk_home.clone() + "/build/cmake/android.toolchain.cmake";
+        let android_abi = android_abi_name(&target_triple).unwrap();
+        let min_sdk_version = "android-29";
+
+        config
+            .define("CMAKE_ANDROID_NDK", &ndk_home)
+            .define("ANDROID_NDK", &ndk_home)
+            .define("CMAKE_ANDROID_ARCH_ABI", &android_abi)
+            .define("ANDROID_ABI", &android_abi)
+            // ANDROID_PLATFORM == minSdkVersion / min_sdk_version
+            .define("ANDROID_PLATFORM", &min_sdk_version)
+            .define("CMAKE_TOOLCHAIN_FILE", &cmake_toolchain_file)
+            .define("ANDROID_TOOLCHAIN", "clang")
+            .define("ANDROID_STL", "c++_static")
+            .define("ANDROID_ARM_NEON", "TRUE")
+            .cflag("-flto=thin")
+            .cxxflag("-std=c++20")
+            .cxxflag("-fexceptions")
+            .cxxflag("-frtti")
+            .cxxflag("-flto=thin");
+        if profile == "release" {
+            config.cflag("-O3").cxxflag("-O3");
+        }
+        config.build()
     } else {
         let pkg_config_path = make_ffmpeg_pkg_config_path();
         let build_cuda = cmake_option_from_feature(BUILD_CUDA_INTEROP_FEATURE);
-        let default_generator = "Ninja";
-        let cmake_generator = env::var(CMAKE_GEN_ENV_VAR)
-            .map(|s| {
-                if s.is_empty() {
-                    String::from(default_generator)
-                } else {
-                    s
-                }
-            })
-            .unwrap_or(String::from(default_generator));
-
-        let mut config = Config::new("cpp/ALVR-OpenXR-Engine");
-        if target_triple.vendor != target_lexicon::Vendor::Uwp {
-            assert!(!cmake_generator.is_empty());
-            config.generator(cmake_generator);
-        } else {
-            // Using Ninja to build UWP/WinStore apps fails to build.
-            // This should not be neccessary if the enviroment is
-            // setup correctly (i.e. using vcvarsall.bat) for building UWP/WinStore apps,
-            // and VS's fork of cmake.
-            define_windows_store(&mut config);
-        }
         config
-            .always_configure(true)
+            .define("BUILD_LOADER", "ON")
             .define(CMAKE_PREFIX_PATH_VAR, &pkg_config_path)
             .define("BUILD_CUDA_INTEROP", build_cuda)
-            .define("BUILD_ALL_EXTENSIONS", "ON")
-            .define("BUILD_API_LAYERS", "OFF")
-            .define("BUILD_TESTS", "OFF")
-            .define("BUILD_CONFORMANCE_TESTS", "OFF")
-            .define(
-                "USE_OCULUS_OXR_EXT_HEADERS",
-                cmake_option_from_feature(&ENABLE_OCULUS_EXT_HEADERS_FEATURE),
-            )
-            .define(
-                "DISABLE_DECODER_SUPPORT",
-                cmake_option_from_feature(&DISABLE_DECODER_FEATURE),
-            )
             .build()
     };
 
@@ -254,7 +244,6 @@ fn main() {
     } else {
         ""
     };
-
     let tracking_binding_path = alvr_client_dir.join("android/app/src/main/cpp");
     let binding_file = alxr_engine_src_dir.join("alxr_engine/alxr_engine.h");
     bindgen::builder()
@@ -277,51 +266,44 @@ fn main() {
         .write_to_file(out_dir.join("alxr_engine.rs"))
         .expect("alxr_engine.rs");
 
-    if is_android_env(&target_triple) {
-        println!("\npath: {0}\n", alxr_engine_output_dir.to_string_lossy());
-        println!(
-            "cargo:rustc-link-search=native={0}",
-            alxr_engine_output_dir.to_string_lossy()
-        );
-    } else {
-        let alxr_engine_bin_dir = alxr_engine_output_dir.join("bin");
-        let alxr_engine_lib_dir = alxr_engine_output_dir.join("lib");
-        println!(
-            "cargo:rustc-link-search=native={0}",
-            alxr_engine_lib_dir.to_string_lossy()
-        );
-        println!(
-            "cargo:rustc-link-search=native={0}",
-            alxr_engine_bin_dir.to_string_lossy()
-        );
+    let alxr_engine_bin_dir = alxr_engine_output_dir.join("bin");
+    let alxr_engine_lib_dir = alxr_engine_output_dir.join("lib");
 
-        if cfg!(target_os = "windows") {
-            let mut run_exe_dir = out_dir.clone();
-            run_exe_dir.pop();
-            run_exe_dir.pop();
-            run_exe_dir.pop();
+    if cfg!(target_os = "windows") {
+        let mut run_exe_dir = out_dir.clone();
+        run_exe_dir.pop();
+        run_exe_dir.pop();
+        run_exe_dir.pop();
 
-            fn is_cso_file(path: &std::path::Path) -> bool {
-                if let Some(ext) = path.extension() {
-                    if ext.to_str().unwrap().eq("cso") {
-                        return true;
-                    }
+        fn is_cso_file(path: &std::path::Path) -> bool {
+            if let Some(ext) = path.extension() {
+                if ext.to_str().unwrap().eq("cso") {
+                    return true;
                 }
-                return false;
             }
-            for cso_file in walkdir::WalkDir::new(&alxr_engine_bin_dir)
-                .into_iter()
-                .filter_map(|maybe_entry| maybe_entry.ok())
-                .map(|entry| entry.into_path())
-                .filter(|entry| is_cso_file(&entry))
-            {
-                let relative_csof = cso_file.strip_prefix(&alxr_engine_bin_dir).unwrap();
-                let dst_file = run_exe_dir.join(relative_csof);
-                std::fs::create_dir_all(dst_file.parent().unwrap()).unwrap();
-                std::fs::copy(&cso_file, &dst_file).unwrap();
-            }
+            return false;
         }
-    };
+        for cso_file in walkdir::WalkDir::new(&alxr_engine_bin_dir)
+            .into_iter()
+            .filter_map(|maybe_entry| maybe_entry.ok())
+            .map(|entry| entry.into_path())
+            .filter(|entry| is_cso_file(&entry))
+        {
+            let relative_csof = cso_file.strip_prefix(&alxr_engine_bin_dir).unwrap();
+            let dst_file = run_exe_dir.join(relative_csof);
+            std::fs::create_dir_all(dst_file.parent().unwrap()).unwrap();
+            std::fs::copy(&cso_file, &dst_file).unwrap();
+        }
+    }
+
+    println!(
+        "cargo:rustc-link-search=native={0}",
+        alxr_engine_lib_dir.to_string_lossy()
+    );
+    println!(
+        "cargo:rustc-link-search=native={0}",
+        alxr_engine_bin_dir.to_string_lossy()
+    );
 
     if target_triple.operating_system != OperatingSystem::Windows {
         println!("cargo:rustc-link-lib=dylib={0}", "openxr_loader");
